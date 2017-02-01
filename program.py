11622d82c845b720cae9c86dba00a0559c472f20
Jim Bennett <buildcreativewebsites@gmail.com>
2017-02-01 00:32:24 -0800

    updating templates

--- program.py
+++ program.py
@@ -34,6 +34,35 @@ def showRecipeGroups():
     return render_template('recipe-groups.html', recipe_groups=recipe_groups)
 
 
+# Create a new recipe group
+@app.route('/recipe-groups/new/', methods=['GET', 'POST'])
+def newRecipeGroup():
+    if request.method == 'POST':
+        newRecipeGroup = RecipeGroup(name=request.form['name'])
+        session.add(newRecipeGroup)
+        session.commit()
+        return redirect(url_for('showRecipeGroups'))
+    else:
+        return render_template('new-recipe-group.html')
+        # return "This page will be for making a new restaurant"
+
+
+# edit recipe group
+@app.route('/recipe-groups/<int:recipe_group_id>/edit/', methods=['GET', 'POST'])
+def editRecipeGroup(recipe_group_id):
+    editedRecipeGroup = session.query(
+        RecipeGroup).filter_by(id=recipe_group_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedRecipeGroup.name = request.form['name']
+            return redirect(url_for('showRecipeGroups'))
+    else:
+        return render_template(
+            'edit-recipe-group.html', recipe_group=editedRecipeGroup)
+
+
+
+
 # show recipes in group
 @app.route('/recipe-group/<int:recipe_group_id>/')
 @app.route('/recipe-group/<int:recipe_group_id>/recipes/')
